cd1de7d1e8d28efa31e6b48ae457fe41ebeb38c7
Abdoul Anzize Daouda <anzizdaouda0@gmail.com>
2026-02-23 11:46:32 +0100

    feat: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  worker: {
+    format: 'es',
+    plugins: () => [react()]
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'ui': ['lucide-react']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['onnxruntime-web']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+});
